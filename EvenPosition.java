a5c85212dd5d42edc8bf470ca6d493e97da18db4
Devika18 <devikankarpe@gmail.com>
2022-09-27 11:23:14 +0530

    added array.java files

---
+++ EvenPosition.java
@@ -0,0 +1,14 @@
+//Java program to print elements of an array present on even position
+
+public class EvenPosition {
+    public static void main(String[] args) {
+        int [] a = new int [] {10,20,30,40,50};
+        System.out.println("Elements of array present on even position are: ");
+
+        //Initializing for loop through the array by increamenting value of i by 2
+        for(int i=1;i<a.length;i=i+2)
+        {
+            System.out.println(a[i]);
+        }
+    }
+}
